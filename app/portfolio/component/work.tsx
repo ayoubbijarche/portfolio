ad576b9b93d0b8fa8a903debd0c1478bfafa1e3d
ayoubbijarche <ayoubbijarche5@gmail.com>
2024-07-27 00:03:37 +0100

    uplaoded images

--- app/portfolio/component/work.tsx
+++ app/portfolio/component/work.tsx
@@ -1,8 +1,15 @@
 import { Code } from "@nextui-org/code";
 import {Card, CardHeader, CardBody} from "@nextui-org/card";
-import { Image } from "@nextui-org/image";
+import {Image} from "@nextui-org/image";
 import { Link } from "@nextui-org/link";
-
+import fashion from "../../../assets/images/fashion.png"; 
+import cli from "../../../assets/images/cli.png"; 
+import mobile from "../../../assets/images/mobile.png"; 
+import posters from "../../../assets/images/posters.png"; 
+import rustchat from "../../../assets/images/rustchat.png"; 
+import sveltechat from "../../../assets/images/sveltechat.png"; 
+import automation from "../../../assets/images/automation.png"; 
+import portfolio from "../../../assets/images/portfolio.png"; 
 
 const ListWork = ({url , img , title , tech} : {url:any , img:any , title:any , tech:any}) => {
   return(
@@ -41,7 +48,7 @@ const Work = () => {
         <Code color="success">@client:~$ sudo ShowWork</Code>
       </div>
        <div className="flex flex-col gap-12 md:flex-row  flex-grow"> 
-        <ListWork title={"Full stack online fashion store"} tech={"using nextjs + pocketbase"} url={"https://github.com/ayoubbijarche/fashion-store-sample"} img={"https://nextui.org/images/hero-card-complete.jpeg"}/>
+        <ListWork title={"Full stack online fashion store"} tech={"using nextjs + pocketbase"} url={"https://github.com/ayoubbijarche/fashion-store-sample"} img={fashion}/>
         <ListWork title={"A full stack chat app sample"} tech={"using svelte + pocketbase"} url={"https://github.com/ayoubbijarche/fashion-store-sample"} img={"https://nextui.org/images/hero-card-complete.jpeg"}/>
         <ListWork title={"A full stack mobile app online store"} tech={"flutter + pocketbase"} url={"https://github.com/ayoubbijarche/fashion-store-sample"} img={"https://nextui.org/images/hero-card-complete.jpeg"}/>
         <ListWork title={"Full stack rooms chat app using rust"} tech={"using rust rocket framework"} url={"https://github.com/ayoubbijarche/fashion-store-sample"} img={"https://nextui.org/images/hero-card-complete.jpeg"}/>
