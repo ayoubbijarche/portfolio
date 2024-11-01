8e96ad401973dd12ed459344362a9564dd21567c
ayoubbijarche <ayoubbijarche6@gmail.com>
2024-11-01 10:11:22 +0100

    setting up the for github pages

--- next.config.ts
+++ next.config.ts
@@ -5,13 +5,7 @@ const nextConfig: NextConfig = {
   devIndicators: {
     appIsrStatus: false,
   },
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
-  trailingSlash: true,
+
 };
 
 export default nextConfig;
